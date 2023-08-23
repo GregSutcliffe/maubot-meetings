5f5bd78b93010cc4b58aef4109799c8c6b22953f
Ryan Lerch <rlerch@redhat.com>
2023-08-23 10:16:46 +1000

    Upload logs as textfiles for Fedora
    Just for now, upload the logs and action items as textfiles for the
    fedora backend
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- meetings/fedora.py
+++ meetings/fedora.py
@@ -2,4 +2,11 @@ async def startmeeting(meetbot, event):
   meetbot.log.info(f'Fedora: Meeting started in {event.room_id}')
 
 async def endmeeting(meetbot, event, meeting_id):
+  full_log    = await meetbot.get_items(meeting_id)
+  info_list   = await meetbot.get_items(meeting_id, "info")
+  action_list = await meetbot.get_items(meeting_id, "action")
+
+  await meetbot.upload_file(event, info_list, "info_items.txt")
+  await meetbot.upload_file(event, action_list, "action_items.txt")
+  await meetbot.upload_file(event, full_log, "full_log.txt")
   meetbot.log.info(f'Fedora: Meeting ended in {event.room_id}')
