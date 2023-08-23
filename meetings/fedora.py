fb8ff96d0a67fcf739293188562125a879aa0255
Ryan Lerch <rlerch@redhat.com>
2023-08-23 10:39:41 +1000

    Add utility to get the room_alias
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- meetings/fedora.py
+++ meetings/fedora.py
@@ -1,7 +1,12 @@
+from .util import get_room_alias
+
 async def startmeeting(meetbot, event):
-  meetbot.log.info(f'Fedora: Meeting started in {event.room_id}')
+  room_alias = await get_room_alias(meetbot.client, event.room_id)
+  meetbot.log.info(f'Fedora: Meeting started in {room_alias}')
 
 async def endmeeting(meetbot, event, meeting_id):
+  room_alias = await get_room_alias(meetbot.client, event.room_id)
+  
   full_log    = await meetbot.get_items(meeting_id)
   info_list   = await meetbot.get_items(meeting_id, "info")
   action_list = await meetbot.get_items(meeting_id, "action")
@@ -9,4 +14,4 @@ async def endmeeting(meetbot, event, meeting_id):
   await meetbot.upload_file(event, info_list, "info_items.txt")
   await meetbot.upload_file(event, action_list, "action_items.txt")
   await meetbot.upload_file(event, full_log, "full_log.txt")
-  meetbot.log.info(f'Fedora: Meeting ended in {event.room_id}')
+  meetbot.log.info(f'Fedora: Meeting ended in {room_alias}')
