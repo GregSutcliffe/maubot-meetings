d5e8602df6266640e6872fa9230aa5bdd6833802
Ryan Lerch <rlerch@redhat.com>
2023-09-08 13:52:32 +1000

    Add command to change meetingname
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- meetings/backends/fedora/__init__.py
+++ meetings/backends/fedora/__init__.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from ...util import get_room_alias, time_from_timestamp
 from maubot.loader import BasePluginLoader
+from slugify import slugify
 
 async def startmeeting(meetbot, event):
     room_alias = await get_room_alias(meetbot.client, event.room_id)
@@ -34,29 +35,31 @@ def render(meetbot, templatename, **kwargs):
     return j2env.from_string(template.decode()).render(**kwargs)
 
 
-async def endmeeting(meetbot, event, meeting_id):
+async def endmeeting(meetbot, event, meeting):
     room_alias = await get_room_alias(meetbot.client, event.room_id)
-    items = await meetbot.get_items(meeting_id)
-    people_present = await meetbot.get_people_present(meeting_id)
+    items = await meetbot.get_items(meeting['meeting_id'])
+    people_present = await meetbot.get_people_present(meeting['meeting_id'])
+    starttime = time_from_timestamp(items[0]['timestamp'], format="%Y-%m-%d-%H-%M")
+    filename = f"{slugify(meeting['meeting_name'])}.{starttime}"
 
     await meetbot.upload_file(
-        event, "text_log.txt", render(meetbot, "text_log.j2", items=items)
+        event, f"{filename}.log.txt", render(meetbot, "text_log.j2", items=items)
     )
     await meetbot.upload_file(
         event,
-        "html_log.html",
+        f"{filename}.log.html",
         render(meetbot, "html_log.j2", items=items, room=room_alias),
     )
 
     await meetbot.upload_file(
         event,
-        "text_minutes.txt",
+        f"{filename}.txt",
         render(meetbot, "text_minutes.j2", items=items, room=room_alias, people_present=people_present),
     )
 
     await meetbot.upload_file(
         event,
-        "html_minutes.html",
+        f"{filename}.html",
         render(meetbot, "html_minutes.j2", items=items, room=room_alias, people_present=people_present),
     )
 
