0559d88b762111df034dd44c9a138fff64fc6039
Greg Sutcliffe <github@emeraldreverie.org>
2026-08-04 12:19:00 +0100

    Ensure that discourse failures are logged
    Rebased version of #46 from webpigeon - thanks!
    
    Signed-off-by: Greg Sutcliffe <github@emeraldreverie.org>

--- meetings/backends/ansible/__init__.py
+++ meetings/backends/ansible/__init__.py
@@ -81,8 +81,7 @@ async def upload_log_to_discourse(config, log_data, logger):
         txt = f"[full_log.txt|attachment]({r['short_url']})"
         return txt
     else:
-        logger.info(res.status_code)
-        logger.info(res.content)
+        logger.warning(f"error uploading: {res.status_code} - {res.content}")
         return ""
 
 
@@ -99,8 +98,9 @@ async def post_to_discourse(config, raw_post, title, logger):
     if res.status_code == 200:
         r = json.loads(res.content)
         return r["topic_id"]
-    return ""
-
+    else:
+        logger.warning(f"error posting: {res.status_code} - {res.content}")
+        return ""
 
 # required backend methods
 async def startmeeting(meetbot, event, meeting):
